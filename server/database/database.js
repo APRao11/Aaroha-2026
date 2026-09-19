253cdbab828b23ccda5913f912e16f38b69c51d1
Thushara B S <abhibajimar07@gmail.com>
2026-09-19 15:55:14 +0530

    Add assessment results table

--- server/database/database.js
+++ server/database/database.js
@@ -6,7 +6,17 @@ db.exec(`
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     email TEXT
-  )
+  ),
+  CREATE TABLE IF NOT EXISTS assessment_results (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    learner_id INTEGER NOT NULL,
+    skill TEXT NOT NULL,
+    correct_answers INTEGER NOT NULL,
+    total_questions INTEGER NOT NULL,
+    proficiency REAL NOT NULL,
+    skill_gap REAL NOT NULL,
+    FOREIGN KEY (learner_id) REFERENCES learners(id)
+    )
 `);
 
 console.log("Database connected successfully!");
